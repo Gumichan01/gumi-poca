fc40861aceda62ae00a4321f4569679b59b4d783
Luxon <luxon.jean.pierre@gmail.com>
2017-11-07 13:46:21 +0100

    Survey.scala: fix compiling error

--- src/Survey.scala
+++ src/Survey.scala
@@ -5,14 +5,14 @@
 
 class Survey(questionl: List[Question]) {
 
-    if(questionl.isEmpty())
-        throw neW IllegalArgumentException("A survey cannot be empty")
+    if(questionl.isEmpty)
+        throw new IllegalArgumentException("A survey cannot be empty")
 
     // I am still thinking about how I should define and implement it
     /// def check: Boolean = {}
 }
 
-object SMain extends Any {
+object SMain{
 
     def main(args: Array[String]) : Unit = {
 
