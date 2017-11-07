401d40cd726e3a13dc14e04acc2c99333ef44a37
Luxon <luxon.jean.pierre@gmail.com>
2017-11-07 13:43:47 +0100

    Survey.scala: first implementation - not complete

---
+++ src/Survey.scala
@@ -0,0 +1,22 @@
+
+/*
+  Questionnaire
+*/
+
+class Survey(questionl: List[Question]) {
+
+    if(questionl.isEmpty())
+        throw neW IllegalArgumentException("A survey cannot be empty")
+
+    // I am still thinking about how I should define and implement it
+    /// def check: Boolean = {}
+}
+
+object SMain extends Any {
+
+    def main(args: Array[String]) : Unit = {
+
+        // TODO: Test it
+    }
+
+}
