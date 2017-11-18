13a9a6854bea141a4c63033a1220ef4a985c22d6
Luxon JEAN-PIERRE <luxon.jean.pierre@gmail.com>
2017-11-18 16:48:46 +0100

    survey: done - but it seems nasty - review it

--- deliverables/skeleton/Survey.scala
+++ deliverables/skeleton/Survey.scala
@@ -8,22 +8,49 @@ class Survey(questionl: List[Question]) {
     if(questionl.isEmpty)
         throw new IllegalArgumentException("A survey cannot be empty")
 
+    private def getQuestion(ql : List[Question], id: Int) : Question = {
+
+        if(id < 0)
+            throw new IllegalArgumentException("A survey cannot be empty")
+
+        ql match {
+            case Nil  => throw new Exception("hello")
+            case h::q => { if (h.id == id) h else getQuestion(ql,id) }
+        }
+    }
+
     // I am still thinking about how I should define and implement it
     // Return the number of good answers
-    /*def check(answerl: List[Answer]): Int = {
+    def check(answer_sh: AnswerSheet): (Int,Int) = {
 
-        var good = 0
-        check_(answerl, good)
-    }*/
+        var res = (0,0);    // (good_answers, question_to_check_manually)
+        check_(answer_sh.getAnswers, res)
+    }
 
-    /*private def check_(answerl : List[Answer], acc : Int) : Int = {
+    private def check_(answerl : List[(Answer, Int)], acc : (Int,Int)) : (Int,Int) = {
+
+        val (g, nb) = acc
 
         answerl match {
 
-            case h::q => check_(q, (if (questionl(h.id) isGoodAnswer h) (acc + 1) else acc ) )
-            case _    => acc
+            case (a, i)::q => {
+
+                val question = getQuestion(questionl, i)
+
+                question match {
+
+                    case MultipleChoiceQuestion(_,_,_) => {
+
+                        check_(q, (if (question isGoodAnswer List(a)) (g + 1, nb) else acc ))
+                    }
+
+                    case _ => check_(q, (g, nb + 1))
+                }   //match
+            }   // match
+
+            case _  => acc
         }
-    }*/
+    }
 }
 
 object SMain{
