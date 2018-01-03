0e4c7e132652f59b64729cc135479eecfa647b3d
Luxon <luxon.jean.pierre@gmail.com>
2018-01-03 19:34:44 +0100

    Survey: check questions (^_^)// + test it

--- deliverables/version2/evaluator_server/app/model/Survey.scala
+++ deliverables/version2/evaluator_server/app/model/Survey.scala
@@ -27,6 +27,7 @@ abstract class Survey(questionl: List[Question]) {
         "Survey with id = " + id
     }
 }
+/// MCSurvey is not supposed to exist anymore, but I must not remove it (too late!)
 case class MCSurvey(questionl: List[MultipleChoiceQuestion])
     extends Survey(questionl) with WithCheckableQA {
 
@@ -38,7 +39,7 @@ case class MCSurvey(questionl: List[MultipleChoiceQuestion])
 
         ql match {
             case Nil    => throw new Exception("hello")
-            case h :: q => { if (h.id == id) h else getQuestion(ql, id) }
+            case h :: q => { if (h.id == id) h else getQuestion(q, id) }
         }
     }
 
@@ -65,11 +66,49 @@ case class MCSurvey(questionl: List[MultipleChoiceQuestion])
         } // match
     }
 }
-// TODO Survey that contains every types of questions
+// Survey that can get any type of question
 case class GSurvey(questionl: List[Question])
     extends Survey(questionl) with WithCheckableQA {
-    
-    def check(answer_sh: AnswerSheet) = 0
+
+    protected def getQuestion(ql: List[Question], id: Int): Question = {
+
+        if (id < 0)
+            throw new IllegalArgumentException("A survey cannot be empty")
+
+        ql match {
+            case Nil    => throw new Exception("Not found question")
+            case h :: q => { if (h.id == id) h else getQuestion(q, id) }
+        }
+    }
+
+    def check(answer_sh: AnswerSheet) = {
+
+        val acc = 0
+        check_(answer_sh.getAnswers, acc)
+    }
+
+    private def check_(answerl: List[(Answer, Int)], acc: Int): Int = {
+
+        val v = acc
+
+        answerl match {
+
+            case (a, i) :: q => {
+
+                val question = getQuestion(questionl, i)
+
+                question match {
+                    case mcq: MultipleChoiceQuestion =>
+                        check_(q, (if (mcq isGoodAnswer List(a)) (v + 1) else acc))
+
+                    case srcq: CodeSubmissionQuestion =>
+                        check_(q, (if (srcq check a) (v + 1) else acc))
+                }
+            }
+
+            case _ => acc
+        } // match
+    }
 }
 
 object SMain {
@@ -83,6 +122,7 @@ object SMain {
         var ans2 = new TextAnswer("3");
         var ans3 = new TextAnswer("4");
         var ans4 = new TextAnswer("5");
+        var ans5 = new TextAnswer("24");
         // var prof = new Professor("Jean", "Pierre");
         // var student = new Student("Michael","Fail");
 
@@ -90,10 +130,30 @@ object SMain {
         var listGoodAnswer: List[Answer] = List(ans1);
         var question = new MultipleChoiceQuestion(q1, listAnswer, listGoodAnswer);
 
+        val tcase = new TestCase {
+
+            type Input = Int
+            type Expected = Int
+
+            def input: Input = 4
+            def expected: Expected = 24
+        }
+        var srcquestion = new CodeSubmissionQuestion(q1, List(tcase));
+
         val s1 = new MCSurvey(List(question))
         val s2 = new MCSurvey(List(question))
+
+        val l = List(srcquestion) ++: List(question)
+        val s3 = new GSurvey(l)
+        val sh = new AnswerSheet(3)
+
+        sh addAnswer (ans1, question.id)
+        sh addAnswer (ans5, srcquestion.id)
+
         println(s1)
         println(s2)
+        println("check: " + s3.check(sh))
+        println(s3)
     }
 
 }
